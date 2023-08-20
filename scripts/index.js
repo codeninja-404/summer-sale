5c6707efd81848b485500052bf8a481fe34ad6f3
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-08-20 09:31:30 +0600

    added utilities file. for apply btn and purchase btn

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,55 @@
 let total = 0;
+let discount = 0;
+
 function cardClick(data) {
   const itemsListContainer = document.getElementById("new-items");
   const count = itemsListContainer.childElementCount;
   const itemName = data.childNodes[3].childNodes[3].innerText;
-  console.log(itemName);
   const p = document.createElement("p");
   p.innerText = `${count + 1}. ${itemName}`;
   itemsListContainer.appendChild(p);
   const price = data.childNodes[3].childNodes[5].childNodes[0].innerText;
   total = parseFloat(total) + parseFloat(price);
   document.getElementById("total-p").innerText = total.toFixed(2);
-  console.log(total);
+  document.getElementById("checkout").innerText = total.toFixed(2);
+  activeMakePurchase();
+  activeApply();
+}
+
+// function activeMakePurchase() {
+//   const purchase = document.getElementById("purchase");
+//   if (total >= 1) {
+//     purchase.disabled = false;
+//     purchase.classList.add("enabled");
+//   }
+// }
+
+// function activeApply() {
+//   const apply = document.getElementById("apply");
+//   if (total >= 200) {
+//     apply.disabled = false;
+//     apply.classList.add("enabled");
+//   }
+// }
+
+function activeMakePurchase() {
+  condition('purchase',1)
+}
+function activeApply() {
+  condition('apply',200)
+}
+
+
+function discountApply() {
+  const couponField = document.getElementById("coupon");
+  const coupon = couponField.value;
+  if (coupon != "SELL200") {
+    alert("Please enter a valid coupon.");
+  } else {
+    discount = (total * 20) / 100;
+    const finalPrice = total - discount;
+    document.getElementById("discount").innerText = discount.toFixed(2);
+    document.getElementById("checkout").innerText = finalPrice.toFixed(2);
+  }
+  couponField.value = "";
 }
---
+++ scripts/utilities.js
@@ -0,0 +1,8 @@
+function condition (id,number){
+    const btn = document.getElementById(id);
+    if (total >= number) {
+      btn.disabled = false;
+      btn.classList.add("enabled");
+    }
+  }
+  
